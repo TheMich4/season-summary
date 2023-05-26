d93e971c86f7a9deb2d114d83c82581d3675bfca
Michal Dyczkowski <themich4@gmail.com>
2023-05-26 23:49:42 +0200

    Types

--- src/components/member-recap.tsx
+++ src/components/member-recap.tsx
@@ -2,13 +2,18 @@
 
 import { recapKeyToName } from "@/config/recap"
 
-const Stat = ({ name, value }) => {
+type MemberRecapValue = keyof typeof recapKeyToName
+
+const Stat = ({ name, value }: { name: string; value: number }) => {
+  // Omit favorite car and track
   if (typeof value !== "number") return null
 
   return (
     <div className="flex flex-col gap-1 rounded-md border p-2">
       <div className="text-center text-lg font-semibold">{name}:</div>
-      <div className="text-center text-3xl font-bold">{value}</div>
+      <div className="flex h-full items-end justify-center align-bottom text-3xl font-bold">
+        {value}
+      </div>
     </div>
   )
 }
@@ -19,7 +24,11 @@ export const MemberRecap = ({ memberRecap = {} }) => {
   return (
     <div className="grid w-full grid-cols-1 grid-rows-2 gap-2 sm:grid-cols-2 md:grid-cols-7 md:grid-rows-1">
       {Object.entries(memberRecap).map(([key, value]) => (
-        <Stat name={recapKeyToName[key]} value={value} key={key} />
+        <Stat
+          name={recapKeyToName[key as MemberRecapValue]}
+          value={value as number}
+          key={key}
+        />
       ))}
     </div>
   )
