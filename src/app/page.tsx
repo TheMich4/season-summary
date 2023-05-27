a3af0b0e2271fa575eb96a9583535cc1bf3ed947
Michal Dyczkowski <themich4@gmail.com>
2023-05-27 11:05:47 +0200

    Added season switch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,10 @@ export default function IndexPage() {
   return (
     <section className="container grid items-center justify-center gap-6 pb-8 pt-6 md:py-10">
       <div className="flex max-w-[980px] flex-col items-center gap-2">
-        <h1 className="text-3xl font-extrabold leading-tight tracking-tighter sm:text-3xl md:text-5xl lg:text-6xl">
+        <h1 className="text-center text-3xl font-extrabold leading-tight tracking-tighter sm:text-3xl md:text-5xl lg:text-6xl">
           Your iRacing Season Summary
         </h1>
-        <p className="max-w-[700px] text-lg text-muted-foreground sm:text-xl">
+        <p className="max-w-[700px] text-center text-lg text-muted-foreground sm:text-xl">
           See how you did this season!
         </p>
       </div>
--- src/components/season-switch.tsx
+++ src/components/season-switch.tsx
@@ -1,21 +1,64 @@
+"use client"
+
+import { useState } from "react"
 import { ChevronLeft, ChevronRight } from "lucide-react"
 
 import { Button } from "./ui/button"
 
+const currentSeason = { season: 2, year: 2023 }
+
 export const SeasonSwitch = () => {
+  const [{ season, year }, setSeason] = useState(currentSeason)
+
+  const handleSeasonDown = () => {
+    let newSeason = season - 1
+    let newYear = year
+
+    if (newSeason === 0) {
+      newSeason = 4
+      newYear -= 1
+    }
+
+    setSeason({ season: newSeason, year: newYear })
+  }
+
+  const handleSeasonUp = () => {
+    let newSeason = season + 1
+    let newYear = year
+
+    if (newSeason === 5) {
+      newSeason = 1
+      newYear += 1
+    }
+
+    setSeason({ season: newSeason, year: newYear })
+  }
+
   return (
     <div className="flex flex-row items-center justify-center gap-2 rounded-md">
-      <Button size="xs" variant="outline" disabled>
+      <Button
+        size="xs"
+        variant="outline"
+        onClick={handleSeasonDown}
+        disabled={year === 2010 && season === 1}
+      >
         <ChevronLeft className="h-4 w-4" />
       </Button>
 
       <span>
-        <span className="font-bold">2023</span>
+        <span className="font-bold">{year}</span>
         {" Season "}
-        <span className="font-bold">2</span>
+        <span className="font-bold">{season}</span>
       </span>
 
-      <Button size="xs" variant="outline" disabled>
+      <Button
+        size="xs"
+        variant="outline"
+        disabled={
+          currentSeason.season === season && currentSeason.year === year
+        }
+        onClick={handleSeasonUp}
+      >
         <ChevronRight className="h-4 w-4" />
       </Button>
     </div>
