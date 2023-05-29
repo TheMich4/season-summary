52407fc1bc36fce0cb287ab21f68413ef5d9e381
Michal Dyczkowski <themich4@gmail.com>
2023-05-29 19:56:01 +0200

    Added loader to profile

--- src/displays/profile-loading.tsx
+++ src/displays/profile-loading.tsx
@@ -1,3 +1,5 @@
+import { Loader2 } from "lucide-react"
+
 import { SeasonSwitch } from "@/components/season-switch"
 
 export const ProfileLoading = ({
@@ -12,12 +14,16 @@ export const ProfileLoading = ({
   return (
     <div className="flex w-full flex-col gap-2">
       <SeasonSwitch iracingId={iracingId} season={season} year={year} />
-      <div className="align-center flex flex-row gap-2 text-2xl font-semibold">
+      <div className="align-center flex flex-row justify-center gap-2 text-2xl font-semibold">
+        <Loader2 className="h-6 w-6 animate-spin self-center" />
         {"Loading stats for: "}
         <span className="self-center font-extrabold leading-tight tracking-tighter">
           {iracingId}
         </span>
       </div>
+      <span className="text-center text-sm">
+        It can take longer if you have many races this season!
+      </span>
     </div>
   )
 }
