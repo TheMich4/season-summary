24ac330aa2aada1b166c63677cef42e7dea6c637
Michal Dyczkowski <themich4@gmail.com>
2023-05-29 21:27:39 +0200

    Get season from searchParams

--- src/displays/profile.tsx
+++ src/displays/profile.tsx
@@ -1,5 +1,4 @@
-import { env } from "@/env.mjs"
-import IracingAPI from "iracing-api"
+import { getIracingData } from "@/server/get-iracing-data"
 import { Frown } from "lucide-react"
 
 import { Favorite } from "@/components/favorite"
@@ -12,71 +11,11 @@ import { VisitedManager } from "@/components/visited-manager"
 
 interface ProfileProps {
   iracingId: string
-  season?: number
-  year?: number
-}
-
-const getIracingData = async (
-  iracingId: string,
-  year: number,
   season: number
-) => {
-  const ir = new IracingAPI({ timeout: 20000 })
-  await ir.login(env.IRACING_EMAIL, env.IRACING_PASSWORD)
-
-  const customerId = parseInt(iracingId, 10)
-
-  const [memberData, memberRecap, chartData, seasonResults] = await Promise.all(
-    [
-      ir.getMemberData({ customerIds: [customerId] }),
-      ir.getMemberRecap({
-        customerId,
-        year,
-        season,
-      }),
-      ir.getMemberChartData({
-        customerId,
-        chartType: 1,
-        categoryId: 2,
-      }),
-      ir.searchSeries({
-        seasonYear: year,
-        seasonQuarter: season,
-        customerId,
-        officialOnly: true,
-        eventTypes: [5],
-      }),
-    ]
-  )
-
-  const firstRaceDate = new Date(
-    seasonResults?.[0]?.startTime?.split("T")[0] as string
-  )
-  const lastRaceDate = new Date(
-    seasonResults?.[seasonResults?.length - 1]?.startTime?.split(
-      "T"
-    )[0] as string
-  )
-
-  return {
-    memberData: memberData?.members?.[0],
-    memberRecap: memberRecap?.stats,
-    chartData: firstRaceDate
-      ? chartData?.data?.filter(
-          (cd: { when: string; value: number }) =>
-            new Date(cd.when) > firstRaceDate &&
-            new Date(cd.when) < lastRaceDate
-        )
-      : [],
-    seasonResults,
-  }
+  year: number
 }
 
-export const Profile = async ({
-  iracingId,
-  season = 2,
-  year = 2023,
-}: ProfileProps) => {
+export const Profile = async ({ iracingId, season, year }: ProfileProps) => {
   const { memberData, memberRecap, chartData, seasonResults } =
     await getIracingData(iracingId, year, season)
 
