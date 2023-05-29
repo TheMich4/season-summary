94b635146f291c63e9a5f0a9df939b95acbfbaf0
Michal Dyczkowski <themich4@gmail.com>
2023-05-29 21:09:53 +0200

    Added mobile nav menu

---
+++ src/components/mobile-nav-menu.tsx
@@ -0,0 +1,49 @@
+"use client"
+
+import Link from "next/link"
+import { Menu } from "lucide-react"
+
+import { cn } from "@/lib/utils"
+
+import { MainNavProps } from "./main-nav"
+import { Button } from "./ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu"
+
+export const MobileNavMenu = ({ items }: MainNavProps) => {
+  return (
+    <div className="flex self-center md:hidden">
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <Button variant="ghost" size="sm">
+            <Menu />
+          </Button>
+        </DropdownMenuTrigger>
+
+        <DropdownMenuContent align="start">
+          {items?.map(
+            (item, index) =>
+              item.href && (
+                <DropdownMenuItem>
+                  <Link
+                    key={index}
+                    href={item.href}
+                    className={cn(
+                      "p-1",
+                      item.disabled && "cursor-not-allowed opacity-80"
+                    )}
+                  >
+                    {item.title}
+                  </Link>
+                </DropdownMenuItem>
+              )
+          )}
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  )
+}
