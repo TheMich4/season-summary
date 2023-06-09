6ef7df3b9a6091471213876497f7dec7ab290785
Michal Dyczkowski <michal.dyczkowski@bbbit.io>
2023-06-09 20:34:26 +0200

    Added buy me coffee button

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -1,18 +1,30 @@
-import Link from "next/link";
-
-import { siteConfig } from "@/config/site";
-import { buttonVariants } from "@/components/ui/button";
 import { Icons } from "@/components/icons";
+import Link from "next/link";
 import { MainNav } from "@/components/main-nav";
 import { ThemeToggle } from "@/components/theme-toggle";
+import { buttonVariants } from "@/components/ui/button";
+import { siteConfig } from "@/config/site";
 
 export function SiteHeader() {
   return (
-    <header className="sticky top-0 z-40 w-full border-b bg-background">
+    <header className="bg-background sticky top-0 z-40 w-full border-b">
       <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
         <MainNav items={siteConfig.mainNav} />
         <div className="flex flex-1 items-center justify-end space-x-4">
           <nav className="flex items-center space-x-1">
+            <a
+              href="https://www.buymeacoffee.com/dyczkowski"
+              target="_blank"
+              className="h-9"
+              rel="noreferrer"
+            >
+              {/* eslint-disable-next-line @next/next/no-img-element */}
+              <img
+                src="https://cdn.buymeacoffee.com/buttons/v2/default-black.png"
+                alt="Buy Me A Coffee"
+                className="min-w-32 h-9 rounded-md border transition duration-200 ease-in-out hover:scale-105"
+              />
+            </a>
             <Link
               href={siteConfig.links.github}
               target="_blank"
@@ -28,6 +40,7 @@ export function SiteHeader() {
                 <span className="sr-only">GitHub</span>
               </div>
             </Link>
+
             <ThemeToggle />
           </nav>
         </div>
