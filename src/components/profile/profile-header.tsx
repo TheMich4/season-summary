68c03fda5845c6f41e2aec82943d0642e9975ad6
Michal Dyczkowski <themich4@gmail.com>
2023-08-23 16:13:42 +0200

    Profile name alignment

--- src/components/profile/profile-header.tsx
+++ src/components/profile/profile-header.tsx
@@ -21,13 +21,13 @@ export const ProfileHeader = ({
 }: ProfileHeaderProps) => {
   return (
     <div className="w-full grid grid-cols-1 md:grid-cols-3 mb-2 gap-2">
-      <div className="flex flex-col sm:flex-row gap-1 sm:gap-2 justify-self-center md:justify-self-start order-3 md:order-1 text-ellipsis">
-        <span className="text-3xl font-extrabold leading-tight tracking-tighter  text-ellipsis">
+      <div className="flex flex-col sm:flex-row gap-1 sm:gap-2 justify-self-center md:justify-self-start order-3 md:order-1 text-ellipsis items-baseline">
+        <p className="text-3xl font-extrabold leading-tight tracking-tighter">
           {memberData?.displayName ?? ""}
-        </span>
-        <span className="flex items-center justify-center text-end text-sm dark:text-primary text-foreground/80  text-ellipsis">
+        </p>
+        <p className="flex items-center justify-center self-center sm:self-auto text-sm dark:text-primary text-foreground/80">
           ({memberData?.clubName ?? ""})
-        </span>
+        </p>
       </div>
 
       <div className="order-1 md:order-2 flex justify-center">
