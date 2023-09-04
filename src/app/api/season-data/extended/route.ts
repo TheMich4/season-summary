7d39d18cebe6a2439cccc5e8e4e37645bc5d1113
Michal Dyczkowski <themich4@gmail.com>
2023-09-04 15:22:04 +0200

    Removed try catch blocks from requests

--- src/app/api/season-data/extended/route.ts
+++ src/app/api/season-data/extended/route.ts
@@ -3,31 +3,26 @@ import { NextResponse } from "next/server";
 import { getExtendedSeasonData } from "@/server/extended-data";
 
 export async function GET(request: Request) {
-  try {
-    const { searchParams } = new URL(request.url);
-    const iracingId = searchParams.get("iracingId");
-    const year = searchParams.get("year");
-    const season = searchParams.get("season");
-    const category = searchParams.get("category");
+  const { searchParams } = new URL(request.url);
+  const iracingId = searchParams.get("iracingId");
+  const year = searchParams.get("year");
+  const season = searchParams.get("season");
+  const category = searchParams.get("category");
 
-    if (!iracingId || !year || !season || !category) {
-      return NextResponse.json({ error: "Missing parameters" });
-    }
-
-    const { data: results, error } = await getExtendedSeasonData(
-      iracingId,
-      parseInt(year, 10),
-      parseInt(season, 10),
-      category as Category
-    );
-
-    if (!results || error) {
-      return NextResponse.json({ error: error ?? "Failed getting data" });
-    }
-
-    return NextResponse.json({ results });
-  } catch (e) {
-    console.log(e);
-    return NextResponse.error();
+  if (!iracingId || !year || !season || !category) {
+    return NextResponse.json({ error: "Missing parameters" });
   }
+
+  const { data: results, error } = await getExtendedSeasonData(
+    iracingId,
+    parseInt(year, 10),
+    parseInt(season, 10),
+    category as Category
+  );
+
+  if (!results || error) {
+    return NextResponse.json({ error: error ?? "Failed getting data" });
+  }
+
+  return NextResponse.json({ results });
 }
--- src/app/api/season-data/route.ts
+++ src/app/api/season-data/route.ts
@@ -5,27 +5,22 @@ import { getSeasonData } from "@/server/store/iracing-data";
 export const revalidate = 60 * 60; // 1 hour
 
 export async function GET(request: Request) {
-  try {
-    const { searchParams } = new URL(request.url);
-    const iracingId = searchParams.get("iracingId");
-    const year = searchParams.get("year");
-    const season = searchParams.get("season");
-    const category = searchParams.get("category");
+  const { searchParams } = new URL(request.url);
+  const iracingId = searchParams.get("iracingId");
+  const year = searchParams.get("year");
+  const season = searchParams.get("season");
+  const category = searchParams.get("category");
 
-    if (!iracingId || !year || !season || !category) {
-      return NextResponse.error();
-    }
-
-    const data = await getSeasonData(
-      iracingId,
-      year,
-      season,
-      category as Category
-    );
-
-    return NextResponse.json(data);
-  } catch (e) {
-    console.log(e);
+  if (!iracingId || !year || !season || !category) {
     return NextResponse.error();
   }
+
+  const data = await getSeasonData(
+    iracingId,
+    year,
+    season,
+    category as Category
+  );
+
+  return NextResponse.json(data);
 }
