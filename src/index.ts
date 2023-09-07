5de90cfe319ca632ca1832237e79b402f0ed041d
Michal Dyczkowski <themich4@gmail.com>
2023-09-07 07:12:13 +0200

    Status endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import express, { Express, Request, Response } from "express";
+import {
+  getFullData,
+  isFullDataFetched,
+  isFullDataFetching,
+} from "./iracing/data/full-data.js";
 
 import dotenv from "dotenv";
-import { getFullData } from "./iracing/data/full-data.js";
 import { getFullSeasonData } from "./iracing/api/full-data.js";
 
 dotenv.config();
@@ -58,6 +62,39 @@ app.get("/get-full-data", async (req: Request, res: Response) => {
     year,
     season,
     categoryId,
+  });
+});
+
+app.get("/get-full-data-status", (req: Request, res: Response) => {
+  const iracingId = req.query.iracingId as string;
+  const year = req.query.year as string;
+  const season = req.query.season as string;
+  const categoryId = req.query.categoryId as string;
+
+  if (!iracingId || !year || !season || !categoryId) {
+    res.send({ error: "missing params" });
+    return;
+  }
+
+  const isFetched = isFullDataFetched({
+    customerId: iracingId,
+    year: year,
+    season: season,
+    categoryId: categoryId,
+  });
+
+  const isFetching = isFullDataFetching({
+    customerId: iracingId,
+    year: year,
+    season: season,
+    categoryId: categoryId,
+  });
+
+  res.send({
+    error: null,
+    params: { iracingId, year, season, categoryId },
+    isFetched,
+    isFetching,
   });
 });
 
