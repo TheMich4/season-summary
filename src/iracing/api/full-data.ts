28083d3c4c84e0ce1c5802f84ce0f598c4b8e967
Michal Dyczkowski <themich4@gmail.com>
2023-09-08 07:10:22 +0200

    Added error protection

--- src/iracing/api/full-data.ts
+++ src/iracing/api/full-data.ts
@@ -16,77 +16,113 @@ export const getFullSeasonData = async ({
   season,
   categoryId,
 }: FullDataParams) => {
-  console.log("getFullSeasonData", customerId, year, season, categoryId);
+  try {
+    console.log("getFullSeasonData", customerId, year, season, categoryId);
 
-  if (!customerId || !year || !season || !categoryId) {
-    return null;
-  }
+    if (!customerId || !year || !season || !categoryId) {
+      return null;
+    }
 
-  if (isFullDataFetched({ customerId, year, season, categoryId })) {
-    console.log("returning already fetched data");
-    return {
-      data: getFullData({ customerId, year, season, categoryId }),
-      error: null,
-    };
-  }
+    if (isFullDataFetched({ customerId, year, season, categoryId })) {
+      console.log("returning already fetched data");
+      return {
+        data: getFullData({ customerId, year, season, categoryId }),
+        error: null,
+      };
+    }
 
-  if (isFullDataFetching({ customerId, year, season, categoryId })) {
-    console.log("data is still fetching");
-    return {
-      data: null,
-      error: "FETCHING",
-    };
-  }
+    if (isFullDataFetching({ customerId, year, season, categoryId })) {
+      console.log("data is still fetching");
+      return {
+        data: null,
+        error: "FETCHING",
+      };
+    }
 
-  const ir = await getLoggedInIracingAPIClient();
+    const ir = await getLoggedInIracingAPIClient();
 
-  setFullDataFetching({ customerId, year, season, categoryId });
+    setFullDataFetching({ customerId, year, season, categoryId });
 
-  const races = await ir.searchSeries({
-    seasonYear: parseInt(year),
-    seasonQuarter: parseInt(season),
-    customerId: parseInt(customerId),
-    officialOnly: true,
-    eventTypes: [5],
-    categoryIds: [parseInt(categoryId, 10)],
-  });
+    const races = await ir.searchSeries({
+      seasonYear: parseInt(year),
+      seasonQuarter: parseInt(season),
+      customerId: parseInt(customerId),
+      officialOnly: true,
+      eventTypes: [5],
+      categoryIds: [parseInt(categoryId, 10)],
+    });
 
-  if (!races?.length) {
+    if (!races?.length) {
+      setFullData(
+        { customerId, year, season, categoryId },
+        {
+          data: [],
+          isFetching: false,
+          isFetched: true,
+        }
+      );
+      return { data: [], error: "NO_DATA" };
+    }
+
+    console.log(
+      `Getting ${races.length} races for`,
+      customerId,
+      year,
+      season,
+      categoryId
+    );
+
+    let results = [];
+
+    for (const race of races) {
+      const { subsessionId } = race;
+
+      const result = await getRaceResult(subsessionId);
+
+      if (result && result.track.categoryId?.toString() === categoryId) {
+        results.push(result);
+      }
+
+      await new Promise((resolve) => setTimeout(resolve, 3000));
+    }
+
     setFullData(
       { customerId, year, season, categoryId },
       {
-        data: [],
+        data: results,
         isFetching: false,
         isFetched: true,
       }
     );
-    return { data: [], error: "NO_DATA" };
+
+    console.log(
+      "!!! done getting full data",
+      customerId,
+      year,
+      season,
+      categoryId
+    );
+
+    return { data: results, error: null };
+  } catch (e) {
+    console.log(
+      "!!! failed getting full seasonData for",
+      customerId,
+      year,
+      season,
+      categoryId
+    );
+    console.log(e);
+
+    setFullData(
+      { customerId, year, season, categoryId },
+      {
+        data: null,
+        isFetching: false,
+        isFetched: false,
+      }
+    );
+
+    return { data: null, error: e };
   }
-
-  let results = [];
-
-  for (const race of races) {
-    const { subsessionId } = race;
-
-    const result = await getRaceResult(subsessionId);
-
-    if (result && result.track.categoryId?.toString() === categoryId) {
-      results.push(result);
-    }
-
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-  }
-
-  setFullData(
-    { customerId, year, season, categoryId },
-    {
-      data: results,
-      isFetching: false,
-      isFetched: true,
-    }
-  );
-
-  console.log("done getting full data", customerId, year, season, categoryId);
-
-  return { data: results, error: null };
 };
--- src/iracing/api/results.ts
+++ src/iracing/api/results.ts
@@ -1,26 +1,34 @@
+import { getResult, setResult } from "../data/results.js";
+
 import { getLoggedInIracingAPIClient } from "../client.js";
-import { getResult } from "../data/results.js";
 
 export const getRaceResult = async (subsessionId: string) => {
-  console.log("getRaceResult", subsessionId);
+  try {
+    // console.log("getRaceResult", subsessionId);
 
-  const savedResult = getResult(subsessionId);
+    const savedResult = getResult(subsessionId);
 
-  if (savedResult) {
-    console.log("returning saved result", subsessionId);
-    return savedResult;
+    if (savedResult) {
+      console.log("returning saved result", subsessionId);
+      return savedResult;
+    }
+
+    const ir = await getLoggedInIracingAPIClient();
+
+    const result = await ir.getResult({
+      subsessionId: parseInt(subsessionId, 10),
+    });
+
+    // Get only race results
+    if (result?.eventType !== 5) {
+      return null;
+    }
+
+    setResult(subsessionId, result);
+
+    return result;
+  } catch (e) {
+    console.log("getRaceResult", subsessionId, e);
+    return { error: e };
   }
-
-  const ir = await getLoggedInIracingAPIClient();
-
-  const result = await ir.getResult({
-    subsessionId: parseInt(subsessionId, 10),
-  });
-
-  // Get only race results
-  if (result?.eventType !== 5) {
-    return null;
-  }
-
-  return result;
 };
