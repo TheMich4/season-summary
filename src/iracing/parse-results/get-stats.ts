a1ea499253f0ad60188570fd6020aa04a1f89e0b
Michal Dyczkowski <themich4@gmail.com>
2023-09-17 13:40:57 +0200

    Fixed laps count

--- src/iracing/parse-results/get-stats.ts
+++ src/iracing/parse-results/get-stats.ts
@@ -1,6 +1,8 @@
 export const getStats = (currentStats: any, raceResult: any, index: number) => {
   return {
-    laps: currentStats.laps + (raceResult.lapsComplete >= 0 ? 1 : 0),
+    laps:
+      currentStats.laps +
+      (raceResult.lapsComplete > 0 ? raceResult.lapsComplete : 0),
     races: index + 1,
     top5: currentStats.top5 + (raceResult.finishPosition < 5 ? 1 : 0),
     wins: currentStats.wins + (raceResult.finishPosition === 1 ? 1 : 0),
