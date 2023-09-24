98a27290d53724d93570628729bb8d71597290f0
Michal Dyczkowski <themich4@gmail.com>
2023-09-24 15:36:01 +0200

    Signed in dropdown

--- src/components/nav/auth-button.tsx
+++ src/components/nav/auth-button.tsx
@@ -1,6 +1,14 @@
 "use client";
 
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
 import { signIn, signOut, useSession } from "next-auth/react";
 
 import { Button } from "../ui/button";
@@ -13,11 +21,24 @@ export const AuthButton = () => {
   if (!sessionData) return <Button onClick={() => signIn()}>Sign in</Button>;
 
   return (
-    <Avatar className="h-8 w-8 cursor-pointer" onClick={() => signOut()}>
-      <AvatarImage src={sessionData.user?.image ?? ""} />
-      <AvatarFallback>
-        {sessionData.user?.name?.charAt(0) ?? "U"}
-      </AvatarFallback>
-    </Avatar>
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <Avatar className="h-8 w-8 cursor-pointer">
+          <AvatarImage src={sessionData.user?.image ?? ""} />
+          <AvatarFallback>
+            {sessionData.user?.name?.charAt(0) ?? "U"}
+          </AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuLabel>
+          {sessionData.user?.name ?? "My Account"}
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>Settings</DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem onClick={() => signOut()}>Sign out</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 };
