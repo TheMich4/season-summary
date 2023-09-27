80e9bf7019a58db6119afd83c95d97dc1bfcdc24
Michal Dyczkowski <themich4@gmail.com>
2023-09-27 20:52:04 +0200

    Fix build issues

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,18 +2,15 @@ import GithubProvider from "next-auth/providers/github";
 import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
-import { env } from "@/env.mjs";
 
 const prisma = new PrismaClient();
 
-export const authOptions = {
+const authOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
-      // clientId: env.GITHUB_ID,
-      // clientSecret: env.GITHUB_SECRET,
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
     }),
   ],
 };
