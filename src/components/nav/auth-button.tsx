4eb1113fd1a2b99b99b678ee8c2ee7c04cba098b
Michal Dyczkowski <themich4@gmail.com>
2023-11-21 19:11:20 +0100

    Refactor auth button component

--- src/components/nav/auth-button.tsx
+++ src/components/nav/auth-button.tsx
@@ -11,40 +11,44 @@ import {
 } from "../ui/dropdown-menu";
 import { signIn, signOut, useSession } from "next-auth/react";
 
-import { Button } from "../ui/button";
 import Link from "next/link";
+import { User } from "lucide-react";
 
 export const AuthButton = () => {
   const { data: sessionData } = useSession();
-
-  if (!sessionData) return <Button onClick={() => signIn()}>Sign in</Button>;
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger>
         <Avatar className="h-8 w-8 cursor-pointer">
-          <AvatarImage src={sessionData.user?.image ?? ""} />
+          <AvatarImage src={sessionData?.user?.image ?? ""} />
           <AvatarFallback>
-            {sessionData.user?.name?.charAt(0) ?? "U"}
+            {sessionData?.user?.name?.charAt(0) ?? <User className="h-5 w-5" />}
           </AvatarFallback>
         </Avatar>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
-        <DropdownMenuLabel>
-          {sessionData.user?.name ?? "My Account"}
-        </DropdownMenuLabel>
-        <DropdownMenuSeparator />
-
-        <DropdownMenuItem>
-          <Link href="/profile/settings" className="w-full">
-            Settings
-          </Link>
-        </DropdownMenuItem>
-
-        <DropdownMenuSeparator />
-        <DropdownMenuItem onClick={() => signOut()} className="w-full">
-          Sign out
-        </DropdownMenuItem>
+        {sessionData ? (
+          <>
+            <DropdownMenuLabel>
+              {sessionData.user?.name ?? "My Account"}
+            </DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              <Link href="/profile/settings" className="w-full">
+                Settings
+              </Link>
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem onClick={() => signOut()} className="w-full">
+              Sign out
+            </DropdownMenuItem>
+          </>
+        ) : (
+          <DropdownMenuItem onClick={() => signIn()} className="w-full">
+            Sign in
+          </DropdownMenuItem>
+        )}
       </DropdownMenuContent>
     </DropdownMenu>
   );
