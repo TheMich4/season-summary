07f303cd6e562ea81f375d9be37e3fff9905d319
Michal Dyczkowski <themich4@gmail.com>
2023-12-05 18:49:51 +0100

    Update PostHog initialization

--- src/components/providers/ph-provider.tsx
+++ src/components/providers/ph-provider.tsx
@@ -3,13 +3,12 @@
 import { usePathname, useSearchParams } from "next/navigation";
 
 import { PostHogProvider } from "posthog-js/react";
-import { env } from "@/env.mjs";
 import posthog from "posthog-js";
 import { useEffect } from "react";
 
 if (typeof window !== "undefined") {
-  posthog.init(env.NEXT_PUBLIC_POSTHOG_KEY, {
-    api_host: env.NEXT_PUBLIC_POSTHOG_HOST,
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST!,
     capture_pageview: false, // Disable automatic pageview capture, as we capture manually
   });
 }
