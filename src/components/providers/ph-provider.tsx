e57e9536767cf47fafd386e655f88b0bb0b23eec
Michal Dyczkowski <themich4@gmail.com>
2023-12-06 19:38:17 +0100

    Disable automatic pageview capture

--- src/components/providers/ph-provider.tsx
+++ src/components/providers/ph-provider.tsx
@@ -9,7 +9,7 @@ import { useEffect } from "react";
 if (typeof window !== "undefined") {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
     api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST!,
-    capture_pageview: false, // Disable automatic pageview capture, as we capture manually
+    // capture_pageview: false, // Disable automatic pageview capture, as we capture manually
   });
 }
 
