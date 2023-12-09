231402fe035c3db8526358c5ab645c2cd78a519e
Michal Dyczkowski <themich4@gmail.com>
2023-12-09 18:52:56 +0100

    Refactor CarStats component to use Dialog component

--- src/components/extended/car-stats.tsx
+++ src/components/extended/car-stats.tsx
@@ -1,5 +1,13 @@
 "use client";
 
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "../ui/dialog";
+
 import { useMemo } from "react";
 
 interface CarStatsProps {
@@ -8,46 +16,74 @@ interface CarStatsProps {
   };
 }
 
+const Data = ({
+  count,
+  data,
+}: {
+  count: number;
+  data: Array<{
+    carName: string;
+    numberOfRaces: number;
+  }>;
+}) => {
+  return (
+    <div className="flex flex-col text-start">
+      {data.map(({ carName, numberOfRaces }) => (
+        <div
+          className="flex w-full flex-row items-baseline gap-1"
+          key={carName}
+        >
+          <p className="min-w-[19px] font-bold dark:text-primary">
+            {numberOfRaces}
+          </p>
+          <p className="text-xs text-muted-foreground">in</p>
+          <p className="text-sm">{carName}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
 export const CarStats = ({ racesPerCar }: CarStatsProps) => {
-  const { count, data } = useMemo(() => {
+  const { count, data, slicedData } = useMemo(() => {
+    const data = Object.entries(racesPerCar)
+      .map(([carName, numberOfRaces]) => {
+        return {
+          carName,
+          numberOfRaces,
+        };
+      })
+      .sort((a, b) => b.numberOfRaces - a.numberOfRaces);
     return {
       count: Object.values(racesPerCar).length,
-      data: Object.entries(racesPerCar)
-        .map(([carName, numberOfRaces]) => {
-          return {
-            carName,
-            numberOfRaces,
-          };
-        })
-        .sort((a, b) => b.numberOfRaces - a.numberOfRaces)
-        .slice(0, 5),
+      data,
+      slicedData: data.slice(0, 5),
     };
   }, [racesPerCar]);
 
   return (
-    <div className="flex w-full flex-col gap-2 rounded-md border bg-background/40 p-4 text-start">
-      <p className="text-2xl font-semibold leading-none tracking-tight">
-        Most raced cars
-      </p>
-      <div className="flex flex-row items-baseline gap-1 text-sm text-muted-foreground">
-        You raced at
-        <p className="font-bold text-foreground dark:text-primary">{count}</p>
-        different tracks this season.
-      </div>
-      <div className="flex flex-col">
-        {data.map(({ carName, numberOfRaces }) => (
-          <div
-            className="flex w-full flex-row items-baseline gap-1"
-            key={carName}
-          >
-            <p className="min-w-[19px] font-bold dark:text-primary">
-              {numberOfRaces}
+    <Dialog>
+      <DialogTrigger>
+        <div className="flex w-full flex-col gap-2 rounded-md border bg-background/40 p-4 text-start hover:border-primary/60">
+          <p className="text-2xl font-semibold leading-none tracking-tight">
+            Most raced cars
+          </p>
+          <div className="flex flex-row items-baseline gap-1 text-sm text-muted-foreground">
+            You raced at
+            <p className="font-bold text-foreground dark:text-primary">
+              {count}
             </p>
-            <p className="text-xs text-muted-foreground">in</p>
-            <p className="text-sm">{carName}</p>
+            different tracks this season.
           </div>
-        ))}
-      </div>
-    </div>
+          <Data count={count} data={slicedData} />
+        </div>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader className="flex flex-col gap-4">
+          <DialogTitle>Most raced cars</DialogTitle>
+          <Data count={count} data={data} />
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
   );
 };
