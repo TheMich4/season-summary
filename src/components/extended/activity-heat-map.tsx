95c19d58d0375deef1e5a1ecb1ff93c7731ee3f6
Michal Dyczkowski <themich4@gmail.com>
2024-01-03 18:51:39 +0100

    Correctly calculate level

--- src/components/extended/activity-heat-map.tsx
+++ src/components/extended/activity-heat-map.tsx
@@ -1,9 +1,30 @@
 "use client";
 
+import { useTheme } from "next-themes";
 import { useMemo } from "react";
-import ActivityCalendar, { type Activity } from "react-activity-calendar";
+import ActivityCalendar, {
+  ThemeInput,
+  type Activity,
+} from "react-activity-calendar";
+
+const THEME: ThemeInput = {
+  light: ["rgba(0,0,0,0.1)", "hsl(47.9, 95.8%, 53.1%)"],
+  dark: ["rgba(0,0,0,1)", "hsl(47.9, 95.8%, 53.1%)"],
+};
+
+const getLevel = (count: number, max: number) => {
+  if (count === 0) return 0;
+  if (count === max) return 4;
+  if (count <= max / 3) return 1;
+  if (count <= 2 * (max / 3)) return 2;
+  if (count < max) return 3;
+
+  return 0;
+};
 
 export const ActivityHeatMap = ({ raceResults }: { raceResults: any[] }) => {
+  const { resolvedTheme } = useTheme();
+
   const data = useMemo(() => {
     const racesPerDate = raceResults.reduce((acc, result) => {
       const date = result.startTime.split("T")[0];
@@ -14,12 +35,27 @@ export const ActivityHeatMap = ({ raceResults }: { raceResults: any[] }) => {
       };
     }, {});
 
+    const max = Math.max(...Object.values(racesPerDate));
+
     return Object.entries(racesPerDate).map(([date, count]) => ({
       date,
       count,
-      level: 4 / count,
+      level: getLevel(count, max),
     })) as Activity[];
   }, [raceResults]);
 
-  return <ActivityCalendar data={data} />;
+  return (
+    <div className="flex w-full flex-col rounded-md border bg-background/40 p-4">
+      <p className="pb-2 text-base font-normal tracking-tight">Activity</p>
+      <ActivityCalendar
+        colorScheme={resolvedTheme}
+        data={data}
+        theme={THEME}
+        weekStart={2}
+        hideColorLegend
+        hideMonthLabels
+        hideTotalCount
+      />
+    </div>
+  );
 };
