ec2399acc964fb0656ba8b85fca9ca81b44cb5e4
Michal Dyczkowski <themich4@gmail.com>
2024-01-06 16:45:27 +0100

    Add Error component to main layout

---
+++ src/app/(main-layout)/error.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useEffect } from "react";
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error);
+  }, [error]);
+
+  return (
+    <div className="flex flex-col items-center justify-center gap-4 p-4">
+      <h1 className="text-4xl font-bold">Something went wrong!</h1>
+      <Button onClick={() => reset()} variant="secondary">
+        Try again
+      </Button>
+    </div>
+  );
+}
