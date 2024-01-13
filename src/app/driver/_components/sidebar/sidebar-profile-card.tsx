5c81c381877b4b1b7259e390e6558ca74366c5cc
Michal Dyczkowski <themich4@gmail.com>
2024-01-13 14:12:09 +0100

    Add profile URL functionality to sidebar profile card

--- src/app/driver/_components/sidebar/sidebar-profile-card.tsx
+++ src/app/driver/_components/sidebar/sidebar-profile-card.tsx
@@ -2,8 +2,10 @@
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
+import { getProfileUrl } from "@/server/get-profile-url";
 import { ChevronRight, User } from "lucide-react";
 import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
 import { useMemo } from "react";
 
 interface SidebarProfileCardProps {
@@ -17,6 +19,9 @@ export const SidebarProfileCard = ({
   iracingId,
   avatarUrl,
 }: SidebarProfileCardProps) => {
+  const router = useRouter();
+  const pathname = usePathname();
+
   const displayName = useMemo(() => {
     const nameSplit = name.split(" ");
     return nameSplit.reduce((acc, curr, i) => {
@@ -26,7 +31,10 @@ export const SidebarProfileCard = ({
     }, "");
   }, [name]);
 
-  const handleClick = async () => {};
+  const handleClick = async () => {
+    const url = iracingId ? await getProfileUrl(`${iracingId}`) : pathname;
+    router.push(url);
+  };
 
   return (
     <div className="flex flex-row items-center justify-between gap-2 rounded-md border bg-background/40 p-2">
