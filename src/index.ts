0f8c8aafa1860111ba989ba2df0ce471a1b6a419
Michal Dyczkowski <themich4@gmail.com>
2024-01-21 12:39:36 +0100

    Refactor code to clear pending season data before starting server

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import { clearPendingSeasonData } from "./db/actions/clear-pending-season-data";
 import { createServer } from "./server";
 
-console.log("Clearing pending season data before starting server");
-await clearPendingSeasonData();
-
 const server = createServer();
 
 console.log(`Listening on localhost:${server.port}`);
+
+console.log("Clearing pending season data");
+await clearPendingSeasonData();
