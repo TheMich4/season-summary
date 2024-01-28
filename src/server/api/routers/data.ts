36e9640a4c2832d08fb6efcfb47817854ccf4fe4
Michal Dyczkowski <themich4@gmail.com>
2024-01-28 18:24:29 +0100

    Refactor getDriverSeasons to use prisma.include and add getDriverSeasons to dataRouter

--- src/server/api/routers/data.ts
+++ src/server/api/routers/data.ts
@@ -1,7 +1,20 @@
+// TODO: Fix those
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
 import { Category, categoryToId } from "@/config/category";
 import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
 import { env } from "process";
 import { z } from "zod";
+
+const omit = (obj: Record<string, any> | undefined, keys: string[]) => {
+  if (!obj) return obj;
+
+  const newObj = { ...obj };
+  keys.forEach((key) => delete newObj[key]);
+  return newObj;
+};
 
 export const dataRouter = createTRPCRouter({
   getData: publicProcedure
@@ -50,4 +63,89 @@ export const dataRouter = createTRPCRouter({
       return undefined;
     }
   }),
+  getDriverSeasons: publicProcedure
+    .input(z.object({ iracingId: z.string() }))
+    .query(async ({ ctx, input }) => {
+      try {
+        const seasonData = await ctx.db.seasonData.findMany({
+          where: { userId: +input.iracingId },
+          select: {
+            id: true,
+            seasonId: true,
+          },
+        });
+
+        const seasons = await ctx.db.season.findMany({
+          where: {
+            id: {
+              in: seasonData.map((season) => season.seasonId),
+            },
+          },
+          select: {
+            id: true,
+            season: true,
+            year: true,
+            category: true,
+          },
+        });
+
+        const data = await ctx.db.data.findMany({
+          where: {
+            seasonDataId: {
+              in: seasonData.map((season) => season.id),
+            },
+          },
+          select: {
+            seasonDataId: true,
+            finalIRating: true,
+            stats: true,
+          },
+        });
+
+        const parsedData = seasonData.map((sd) => {
+          const season = seasons.find((s) => s.id === sd.seasonId) as Record<
+            string,
+            any
+          >;
+          const d = data.find((d) => d.seasonDataId === sd.id) as Record<
+            string,
+            any
+          >;
+
+          return {
+            season: omit(season, ["id"]) as Record<string, any>,
+            data: omit(d, ["seasonDataId"]),
+          };
+        });
+
+        const grouped = parsedData.reduce(
+          (acc, curr) => {
+            const { season, data } = curr;
+            const key = `${season.season}-${season.year}`;
+            const category = categoryToId[season.category as Category];
+
+            if (!category) return acc;
+
+            return {
+              ...acc,
+              [key]: {
+                ...acc[key],
+                [category]: data,
+              },
+            };
+          },
+          {} as Record<string, any>,
+        );
+
+        return Object.values(grouped).sort((a, b) => {
+          if (a.season.year > b.season.year) return -1;
+          if (a.season.year < b.season.year) return 1;
+          if (a.season.season > b.season.season) return -1;
+          if (a.season.season < b.season.season) return 1;
+          return 0;
+        });
+      } catch (err) {
+        return undefined;
+      }
+    }),
 });
