69b10173cb6be09be0aa09421dff1ce1cb444642
Michal Dyczkowski <themich4@gmail.com>
2024-03-17 17:44:42 +0100

    Prisma config

--- packages/prisma/src/index.ts
+++ packages/prisma/src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello via Bun!");
+import type { Context } from "./index.d";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const createContext = async (ctx: any): Promise<Context> => {
+  // Skip if you are not using a serverless environment
+  ctx.callbackWaitsForEmptyEventLoop = false;
+
+  return { ...ctx, prisma };
+};
