5e91e2a711246771b426019735db3b46c16e0014
Michal Dyczkowski <themich4@gmail.com>
2024-03-17 13:04:26 +0100

    Added week to result summary

--- src/app/(main-layout)/result/[subsessionId]/_components/race-summary.tsx
+++ src/app/(main-layout)/result/[subsessionId]/_components/race-summary.tsx
@@ -13,13 +13,13 @@ export const RaceSummary = ({ result }: RaceSummaryProps) => {
     <div className="grid grid-cols-2 gap-2 md:grid-cols-3 lg:grid-cols-6">
       <StatBox label="Laps" value={result.raceSummary.lapsComplete} />
       <StatBox label="SOF" value={result.raceSummary.fieldStrength} />
-      <StatBox label="Laps" value={result.raceSummary.lapsComplete} />
       <StatBox
         label="Avg Lap Time"
         value={getIntervalString(result.raceSummary.averageLap)}
       />
       <StatBox label="Lead Changes" value={result.raceSummary.numLeadChanges} />
       <StatBox label="Cautions" value={result.raceSummary.numCautions} />
+      <StatBox label="Week" value={result.raceWeekNum + 1} />
     </div>
   );
 };
