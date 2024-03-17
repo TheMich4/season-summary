f97940c2fecaada0127e0b470c068469cd9796cd
Michal Dyczkowski <themich4@gmail.com>
2024-03-17 15:44:16 +0100

    Restructure for monorepo

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import { clearPendingSeasonData } from "./db/actions/clear-pending-season-data";
-import { createServer } from "./server";
-
-const server = createServer();
-
-console.log(`Listening on localhost:${server.port}`);
-
-console.log("Clearing pending season data");
-await clearPendingSeasonData();
