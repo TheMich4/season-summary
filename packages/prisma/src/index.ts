2b6d1bbd818d72e809c74f7f04e16385696bb802
Michał Dyczkowski <themich4@gmail.com>
2024-03-18 16:45:58 +0100

    Move prisma files to a package (#59)
    * Add prisma package
    
    * Prisma config
    
    * Rename packages
    
    * Link prisma in server
    
    * Link prisma in app
    
    * Fix prisma export

---
+++ packages/prisma/src/index.ts
@@ -0,0 +1,13 @@
+import type { Context } from "./index.d";
+import { PrismaClient } from "@prisma/client";
+
+export const prisma = new PrismaClient({ log: ["error"] });
+
+export const createContext = async (ctx: any): Promise<Context> => {
+  // Skip if you are not using a serverless environment
+  ctx.callbackWaitsForEmptyEventLoop = false;
+
+  return { ...ctx, prisma };
+};
+
+export { PrismaClient };
