cc9a9b0d90084c76584af9665af4f3d01df28838
Michal Dyczkowski <themich4@gmail.com>
2024-06-05 16:57:22 +0200

    Moved type animation to hook

--- app/src/app/(main-layout)/(home)/_components/title.tsx
+++ app/src/app/(main-layout)/(home)/_components/title.tsx
@@ -1,8 +1,9 @@
 "use client";
 
+import { useTypedTextLoop } from "@/hooks/use-typed-text-loop";
 import { getRandomSeason } from "@/lib/season";
-import { animate, motion, useMotionValue, useTransform } from "framer-motion";
-import { type ReactNode, useEffect, useState } from "react";
+import { motion } from "framer-motion";
+import { useState, type ReactNode } from "react";
 
 const getSeasonString = () => {
   const { season, year } = getRandomSeason();
@@ -10,52 +11,30 @@ const getSeasonString = () => {
   return `${season}${year}`;
 };
 
-export const Title = () => {
-  const isInitialRender = useMotionValue(true);
-  const textIndex = useMotionValue(0);
-  const texts = ["eason", "random"];
-  const baseText = useTransform(textIndex, (latest) =>
-    latest === 0 ? texts[latest] || "" : getSeasonString(),
-  );
-  const count = useMotionValue(0);
-  const rounded = useTransform(count, (latest) => Math.round(latest));
-  const displayText = useTransform(rounded, (latest) =>
-    isInitialRender.get() === true ? texts[0] : baseText.get().slice(0, latest),
-  );
-  const updatedThisRound = useMotionValue(true);
+const texts = ["eason", "random"];
 
+const getBaseText = (index: number) => {
+  if (index === 0) {
+    return texts[index] ?? "";
+  }
+
+  return getSeasonString();
+};
+
+export const Title = () => {
   const [motionClassName, setMotionClassName] = useState("inline-block");
 
-  useEffect(() => {
-    void animate(count, 30, {
-      type: "tween",
-      duration: 5,
-      ease: "easeIn",
-      repeat: Infinity,
-      repeatType: "reverse",
-      repeatDelay: 0.1,
-      onUpdate(latest) {
-        if (isInitialRender.get() === true && latest > 5) {
-          isInitialRender.set(false);
-        }
-        if (updatedThisRound.get() === true && latest > 0) {
-          updatedThisRound.set(false);
-        } else if (updatedThisRound.get() === false && latest === 0) {
-          if (textIndex.get() === texts.length - 1) {
-            textIndex.set(0);
-            setMotionClassName("inline-block");
-          } else {
-            textIndex.set(textIndex.get() + 1);
-            setMotionClassName(
-              "first-letter:text-primary text-2xl sm:text-3xl md:text-4xl lg:text-5xl inline-block font-black",
-            );
-          }
-          updatedThisRound.set(true);
-        }
-      },
-    });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  const displayText = useTypedTextLoop(texts, {
+    getBaseText,
+    onUpdateToInitial: () => {
+      setMotionClassName("inline-block");
+    },
+    onUpdateToNext: () => {
+      setMotionClassName(
+        "first-letter:text-primary text-2xl sm:text-3xl md:text-4xl lg:text-5xl inline-block font-black",
+      );
+    },
+  });
 
   return (
     <h1 className="text-center text-3xl font-extrabold leading-tight tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl">
---
+++ app/src/hooks/use-typed-text-loop.ts
@@ -0,0 +1,54 @@
+import { animate, useMotionValue, useTransform } from "framer-motion";
+import { useEffect } from "react";
+
+interface Config {
+  getBaseText?: (index: number) => string;
+  onUpdateToInitial?: () => void;
+  onUpdateToNext?: () => void;
+}
+
+export const useTypedTextLoop = (texts: Array<string>, config?: Config) => {
+  const isInitialRender = useMotionValue(true);
+  const textIndex = useMotionValue(0);
+  const baseText = useTransform(
+    textIndex,
+    (latest) => config?.getBaseText?.(latest) ?? texts[latest] ?? "",
+  );
+  const count = useMotionValue(0);
+  const rounded = useTransform(count, (latest) => Math.round(latest));
+  const displayText = useTransform(rounded, (latest) =>
+    isInitialRender.get() === true ? texts[0] : baseText.get().slice(0, latest),
+  );
+  const updatedThisRound = useMotionValue(true);
+
+  useEffect(() => {
+    void animate(count, 30, {
+      type: "tween",
+      duration: 5,
+      ease: "easeIn",
+      repeat: Infinity,
+      repeatType: "reverse",
+      repeatDelay: 0.1,
+      onUpdate(latest) {
+        if (isInitialRender.get() === true && latest > 5) {
+          isInitialRender.set(false);
+        }
+        if (updatedThisRound.get() === true && latest > 0) {
+          updatedThisRound.set(false);
+        } else if (updatedThisRound.get() === false && latest === 0) {
+          if (textIndex.get() === texts.length - 1) {
+            textIndex.set(0);
+            config?.onUpdateToInitial?.();
+          } else {
+            textIndex.set(textIndex.get() + 1);
+            config?.onUpdateToNext?.();
+          }
+          updatedThisRound.set(true);
+        }
+      },
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return displayText;
+};
